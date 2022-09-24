18fbd9f0ecff460ca1b88de4eff165521ce8cb2d
Bart Massey <bart.massey@gmail.com>
2022-09-24 12:58:19 -0700

    changed to use resume_unwind

--- cleanerr.rs
+++ cleanerr.rs
@@ -1,8 +1,8 @@
-fn main() -> Result<(), String> {
+fn main() {
     let argv: Vec<String> = std::env::args().collect();
     if argv.len() > 1 {
-        return Err(format!("{}: arguments: {}", &argv[0], argv[1..].join(" ")));
+        eprintln!("{}: arguments: {}", &argv[0], argv[1..].join(" "));
+        std::panic::resume_unwind(Box::new(()));
     }
     println!("no arguments");
-    Ok(())
 }
